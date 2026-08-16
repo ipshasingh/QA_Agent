bb7e4325ca8fbd7408d02c2c704d5b2ac9617b9d
Ipsha Singh <ipsha.singh28@gmail.com>
2026-08-16 11:39:00 +0530

    Backend updated for render

--- server/server.js
+++ server/server.js
@@ -283,7 +283,7 @@ app.get("/api/qa-plans/:id/versions", (req, res) => {
 }); 
 const PORT = process.env.PORT || 5000; 
  
-app.listen(PORT, () => { 
+app.listen(PORT, "0.0.0.0", () => { 
   console.log(`Server running on http://localhost:${PORT}`); 
 });
 
